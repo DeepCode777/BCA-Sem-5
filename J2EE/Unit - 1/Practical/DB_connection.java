e77d7415df28ebde561451bd2421154a9a1aa41f
Code With Deep <deeppoptani777@gmail.com>
2026-07-08 16:09:28 +0530

    Update DB_connection.java

--- J2EE/Unit - 1/Practical/DB_connection.java
+++ J2EE/Unit - 1/Practical/DB_connection.java
@@ -2,11 +2,9 @@ import java.sql.*;
 
 // Database Connectivity with MySQL Using JAVA
 public class DB_connection {
-
 	public static void main(String[] args) {
 		try {
 			//DRIVER :- mysql driver connection
-			
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Driver connect");
 			
@@ -15,7 +13,6 @@ public class DB_connection {
 			
 			//Statement - SQL
 			Statement stmt = con.createStatement();
-			
 			int result = stmt.executeUpdate("insert into std (id,name,age) values (1,'Ashish',45)");
 
 			if(result > 0){
@@ -30,12 +27,10 @@ public class DB_connection {
 			while(rs.next()){
 				System.out.println("ID: "+rs.getInt("id")+" Name: "+rs.getString("name")+" Age: "+rs.getInt("age"));
 			}
-			
 			con.close();	
 		}
 		catch(Exception e) {
 			System.out.println(e);
 		 }
 	}
-
 }
