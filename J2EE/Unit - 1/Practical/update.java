4f2c19ac564d354f32b4d27b4c26acc142fc66f2
Code With Deep <deeppoptani777@gmail.com>
2026-07-15 22:37:27 +0530

    Add files via upload

---
+++ J2EE/Unit - 1/Practical/update.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+
+public class update {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/deep","root","");
+			
+			PreparedStatement pst = con.prepareStatement("update j2ee set name=?, city=? where id=?");
+			
+			pst.setString(1, "Ashish");
+			pst.setString(2, "Amarnagaer");
+			pst.setInt(3, 3);
+			
+			System.out.println(pst.executeUpdate()+ " Recored Upadate");
+			
+			ResultSet rs = pst.executeQuery("select * from j2ee");
+			while(rs.next()) {
+				System.out.println("ID : "+ rs.getInt(1) +"\nName : "+rs.getString(2) +"\nCity : "+ rs.getString(3));
+			}
+			
+			con.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
