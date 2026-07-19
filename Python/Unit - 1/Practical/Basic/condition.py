4eae221eec37e8bd9a6cbf3cfb5fcada7c523568
Code With Deep <deeppoptani777@gmail.com>
2026-07-19 22:59:50 +0530

    Update condition.py

--- Python/Unit - 1/Practical/Basic/condition.py
+++ Python/Unit - 1/Practical/Basic/condition.py
@@ -4,3 +4,11 @@
 A = 10
 if(A > 0):
     print("A is greater then 0")
+
+# 2] if...else statment
+a = 10
+b = 5
+if(a < b):
+    print("A is greater then B")
+else:
+    print("B is greater then A")
