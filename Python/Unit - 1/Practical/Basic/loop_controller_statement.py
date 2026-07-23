be07a1ec87da66c4b874b243272f0fa4327527d8
Code With Deep <deeppoptani777@gmail.com>
2026-07-23 23:23:00 +0530

    Create loop_controller_statement.py

---
+++ Python/Unit - 1/Practical/Basic/loop_controller_statement.py
@@ -0,0 +1,13 @@
+#Loop Controller Statements
+
+# 1] - (pass statment)
+a = int(input("Enter Number : "))
+ print()
+b = int(input("Enter Number : "))
+print()
+if (a>b):
+    pass    # Pass(nathing to do then just can pass) the control if condition true 
+else:
+    print("B is grater")
+
+print("End... pass controller")
