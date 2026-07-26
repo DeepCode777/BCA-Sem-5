8c6440bfff93449bf2392cf3c69b18c506590f3e
Code With Deep <deeppoptani777@gmail.com>
2026-07-26 23:39:01 +0530

    Create advance_fun.py

---
+++ Python/Unit - 1/Practical/Function/advance_fun.py
@@ -0,0 +1,16 @@
+def course (name): # parameter retrive
+    print("Course - "+ name) ; # use parameter
+
+course("BCA") # Argument passing
+course("BBA")
+course("B.com")
+
+#return type
+def sum(x,y):
+    return x+y    #return value
+
+def mult(a,b):
+    return a*b
+
+print(sum(225, 550))
+print(mult(225, 550))
