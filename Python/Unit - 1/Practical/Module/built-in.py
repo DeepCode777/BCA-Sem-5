de813e98c6744f332ed5647d6fbea0637169051a
Code With Deep <deeppoptani777@gmail.com>
2026-07-29 23:05:04 +0530

    Create Module/built-in.py

---
+++ Python/Unit - 1/Practical/Module/built-in.py
@@ -0,0 +1,10 @@
+import math    # bhbuilt-in module of python
+
+print("abs() =", abs(-56))
+print("max() =", max(6,3,8,2))
+print("min() =", min(6,3,8,2))
+print("sqrt(4) =", math.sqrt(4))
+print("pow(2,4) =", math.pow(2,4))
+print("ceil(22.88) =", math.ceil(22.88))
+print("floor(22.88) =", math.floor(22.88))
+print("Sin(90) =", math.sin(math.pi/2))
