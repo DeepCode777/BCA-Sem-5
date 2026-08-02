25f83908f635dff879ee61378e4b1cd024c7ca31
Code With Deep <deeppoptani777@gmail.com>
2026-08-02 23:41:31 +0530

    Create Collection Data Type/Set.py

---
+++ Python/Unit - 1/Practical/Collection Data Type/Set.py
@@ -0,0 +1,11 @@
+# Create a set (duplicates are automatically removed)
+my_set = {1, 2, 2, 3, 4}
+print(my_set)  # Output: {1, 2, 3, 4}
+
+# Add an element
+my_set.add(5)
+print(my_set)  # Output: {1, 2, 3, 4, 5}
+
+# Remove an element
+my_set.remove(1)
+print(my_set)  # Output: {2, 3, 4, 5}
