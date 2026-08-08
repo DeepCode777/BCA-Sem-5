95569347b30408053b800fe824487cc1dde77e58
Code With Deep <deeppoptani777@gmail.com>
2026-08-08 23:13:40 +0530

    Update Inverted_Right_Angled_Triangle.py

--- Python/Unit - 2/practical/Pattern Problems/Inverted_Right_Angled_Triangle.py
+++ Python/Unit - 2/practical/Pattern Problems/Inverted_Right_Angled_Triangle.py
@@ -6,3 +6,9 @@ print("Method 1")
 for i in range(row, 0, -1):
     print("*  " *i)
 print()
+
+print("Method 2")
+for i in range(row, 0, -1):
+    for j in range(i, 0, -1):
+        print("* ", end=" ")
+    print()
