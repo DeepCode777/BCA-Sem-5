6a1cad06bbb390a0083270c8e245e0c83014adde
Code With Deep <deeppoptani777@gmail.com>
2026-08-08 23:08:24 +0530

    Update Right_Angled_Triangle.py

--- Python/Unit - 2/practical/Pattern Problems/Right_Angled_Triangle.py
+++ Python/Unit - 2/practical/Pattern Problems/Right_Angled_Triangle.py
@@ -6,3 +6,9 @@ print("Method 1")
 for i in range(1, row + 1):
     print("*  " *i)
 print()
+
+print("Method 2")
+for i in range(1, row + 1):
+    for j in range(1, i + 1):
+        print("* ", end=" ")
+    print()
