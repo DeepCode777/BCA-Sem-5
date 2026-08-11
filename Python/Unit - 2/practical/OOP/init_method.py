8be62851c3449bb353d5b373984d5d05cb2dca4c
Code With Deep <deeppoptani777@gmail.com>
2026-08-11 23:05:43 +0530

    Create OOP/init_method.py

---
+++ Python/Unit - 2/practical/OOP/init_method.py
@@ -0,0 +1,15 @@
+#Class With __init__ method
+
+class emp():    #class
+    def __init__(self):    #Class Default Method
+        self.id = int(input("Enter ID : " + "\n"))
+        self.name = input("Enter Name : " + "\n")
+        self.age = int(input("Enter Age : " + "\n"))
+
+    def display(self):    #data display
+        print("Employee ID : ", self.id)
+        print("Employee Name : ", self.name)
+        print("Employee Age : ", self.age)
+
+e = emp()    #class - object 
+e.display()    #call methode Of Class
