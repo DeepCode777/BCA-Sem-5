84d37d0cde11773f4184f523fdeb0973d46124c7
Code With Deep <deeppoptani777@gmail.com>
2026-08-11 19:30:55 +0530

    Create Assert.py

---
+++ Python/Unit - 2/practical/Exception/Assert.py
@@ -0,0 +1,9 @@
+#Assert
+num = int(input("Enter Number : "))
+print()
+
+#Assert Messege Show when Number Is Not Positive
+assert num>0, "Number Shoud Be Positive"
+
+#Is Positive Assert messege not show
+print("Number Is Positive ",num)
