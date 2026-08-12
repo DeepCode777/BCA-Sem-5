36e244c5c114cbb474a85951bc4ab0162a0f762d
Code With Deep <deeppoptani777@gmail.com>
2026-08-12 19:32:12 +0530

    Update Single_Inheritance.py

--- Python/Unit - 2/practical/OOP/Inheritance/Single_Inheritance.py
+++ Python/Unit - 2/practical/OOP/Inheritance/Single_Inheritance.py
@@ -15,3 +15,28 @@ obj.func2()    # Child Ckass Method
 # Output
 # Parente Class
 # Child Class
+
+class forAddSub:
+    num1 = int(input("Enter First Number : " + "\n"))
+    num2 = int(input("Enter Second Number : " + "\n"))
+    
+    def add(self):
+        print("Addition : ",self.num1 + self.num2)
+
+    def sub(self):
+        print("Substraction : ",self.num1 - self.num2)
+
+class forMulDivMod(forAddSub):
+    def multi(self):
+        print("Multiplication : ",self.num1 * self.num2)
+    def div(self):
+        print("Division : ",self.num1 / self.num2)
+    def modu(self):
+        print("Modulation : ",self.num1 % self.num2)
+
+cal = forMulDivMod()
+cal.add()
+cal.sub()
+cal.multi()
+cal.div()
+cal.modu()
